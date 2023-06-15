b5e5e8331ea9198bb8a5b4a41d3013df140d24a8
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-06-15 12:37:24 -0700

    edit again on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main use puts to print sentens 
- * and finish with return 0
- * */
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
