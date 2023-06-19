c1f714e61b3b96774f0a98d9ae548085fad2fc08
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-06-19 11:26:02 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print tow digite
+ * Return: 0 value
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a < 10)
+	{
+		b = a + 1;
+		if (a != b)
+		{
+			while (b < 10)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+				b++;
+			}
+		}
+		a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
