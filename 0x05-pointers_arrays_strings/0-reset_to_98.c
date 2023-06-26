d5acac4cb5a527df63b232194131e8b89c66b21f
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-06-26 14:26:31 -0700

    updata the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: integer pointer as parameter
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+
+#endif /*MAIN_H*/
