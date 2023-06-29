b9655166817a4cd76a0fef20ff5dbc00223bb584
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-06-29 06:18:12 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,2 +1,15 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strncpy - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of characters to concatenate.
+ * Return: Pointer to the resulting string `dest`.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
