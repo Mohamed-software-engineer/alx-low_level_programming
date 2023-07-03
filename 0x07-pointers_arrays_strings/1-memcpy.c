4258739139bd7710f52bcae525b45322bf9d4666
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-03 09:31:28 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: array of char
+ * @src: array of char
+ * @n: integer variable
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	return (dest);
+}
