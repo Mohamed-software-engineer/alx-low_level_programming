b99cadbe8992648bbf949d013741be9d8c7aeffa
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-03 11:18:39 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,14 +5,16 @@
  * _strspn -  gets the length of a prefix substring
  * @s: array of char
  * @accept: array of char
- * Return: number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: number of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	 unsigned int count = 0;
-    while (*s != '\0' && strchr(accept, *s) != NULL) {
-        count++;
-        s++;
-    }
-    return count;
+	unsigned int count = 0;
+
+	while (*s != '\0' && strchr(accept, *s) != NULL)
+	{
+		count++;
+		s++;
+	}
+	return (count);
 }
