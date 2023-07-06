cfb95c3086396c7737062beabe0b89486d30f999
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-06 04:28:53 -0700

    finally solve length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,15 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s != '\0')
-	{
-		i++;
-		_strlen_recursion(s + 1);
-	}
 	if (*s == '\0')
 	{
-		return (i);
+		return (0);
 	}
+	return (1 + _strlen_recursion(s + 1));
 }
