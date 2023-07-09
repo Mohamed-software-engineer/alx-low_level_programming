8ef03f9b6185a3a792625b019bd8cc35a2b02da8
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-09 12:03:38 -0700

    fixs betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - print name of program
+ * @argc: counter of arguments
+ * @argv: array of strings
  * Return: 0 value
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
