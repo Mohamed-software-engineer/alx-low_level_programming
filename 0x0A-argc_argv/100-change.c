c18cf30e378b435e14c1af49faa6736fe83ff863
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-10 08:59:36 -0700

    change cents to coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_minimum_coins - get minimum coins
+ * @cents: integer variable
+ * Return: count
+ */
+int get_minimum_coins(int cents)
+{
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+	int count = 0;
+	int i = 0;
+
+	for (; i < num_coins; i++)
+	{
+		count += cents / coins[i];
+		cents %= coins[i];
+	}
+	return (count);
+}
+/**
+ * main - change cents to coin
+ * @argc: counter to arguments
+ * @argv: array of strings
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	int cents = atoi(argv[1]);
+	int min_coins = get_minimum_coins(cents);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	printf("%d\n", min_coins);
+
+	return (0);
+}
