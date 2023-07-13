e1d8094fe2f84a7876c82603f9418bb00065fd15
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-13 08:56:18 -0700

    I don't know

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,14 +5,14 @@
 /**
  * string_nconcat - concatenates two strings.
  * @s1: array of char
- * @s2: array of char 
+ * @s2: array of char
  * @n: unsigned intger variable first byts of s2
  * Return: array of char
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i,j;
+	unsigned int i, j;
 	size_t len_s1 = strlen(s1);
 
 	p = malloc(sizeof(char) * (len_s1 + 1 + n));
