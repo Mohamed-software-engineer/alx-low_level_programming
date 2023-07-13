ee984da6e19905d354113c749d1eeda9b20abc61
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-13 08:38:12 -0700

    try solve second task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: array of char
+ * @s2: array of char 
+ * @n: unsigned intger variable first byts of s2
+ * Return: array of char
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i,j;
+
+	p = malloc(sizeof(char) * (strlen(s1) + 1 + n));
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < n && s2[j] != '\0')
+	{
+		p[i] = s2[j];
+		j++;
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
