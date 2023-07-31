8241d14d25ec1ab21350b22247dbf30dc43e674e
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-07-31 07:40:57 -0700

    solva 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,24 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+
 /**
- * sum_them_all - sum unknowen number
- * @n: integer number
- * Return: integer number
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list arg;
-	unsigned int i = 0;
-	int sum = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	va_start(arg,n);
-	for (; i < n; i++)
-	{
-		int value = va_arg(arg, int);
-		sum += value;
-	}
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
 	va_end(ap);
 
 	return (sum);
