ab8b97369b5056950117ee8d499df015733e88a2
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-08-01 08:09:43 -0700

    solve first task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: parameter of listint_t
+ * Return: number of all elements in listint_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t m = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		m++;
+	}
+	return (m);
+}
