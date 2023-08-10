b2be99b58769831f88575ce8e3fa36f701defa4a
Mohamed-software-engineer <mohamedsaad01011955461@gmial.com>
2023-08-10 07:33:14 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * create_file - creat file
+ * @filename: The name of the file to create.
+ * @text_content: content of file
+ *
+ * Return: 1 to success or -1 to filar
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int w;
+
+	w = write(fd, text_content, strlen(text_content));
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (w == -1)
+	{
+		return (-1);
+	}
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		close(fd);
+	}
+	if (ftruncate(fd, 0) == -1)
+	{
+		close(fd);
+	}
+	return (1);
+}
